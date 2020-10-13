3df13e07a9228f5b2a9fb470551baa917d3f9484
easychessanimations <chessbotroulette@gmail.com>
2020-10-13 12:50:02 +0000

    Dev server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css'
 import './favicon.ico'
 import { div } from './smartdom/smartdom.js'
 
-let app = div().pad(20).w(400).h(400).bc("#ffa").c("#007").html("Smartdom demo")
+let app = div().pad(20).w(400).h(400).bc("#ffa").c("#007").html("Smartdom demo !!")
     .fwb().fs(20).fsti().tdecu()
 
 document.body.appendChild(app.e)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },    
+  devServer: { 
+    contentBase: path.join(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+    watchContentBase: true,    
+    watchOptions: {
+        poll: true
+    },
   },
-  module: {
+  module: {      
      rules: [
         {
           test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
