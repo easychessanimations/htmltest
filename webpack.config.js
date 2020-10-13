2cb3c716be424cc834ac387e23a934d78c170485
easychessanimations <chessbotroulette@gmail.com>
2020-10-13 08:53:43 +0000

    Library output

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    library: "$",
+    libraryTarget: "umd",
   },
   module: {
      rules: [
