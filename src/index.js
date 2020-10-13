848db1a7ebbc6797c1464dbd3023a696fec589e6
easychessanimations <chessbotroulette@gmail.com>
2020-10-13 07:35:55 +0000

    Bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack', 'remote', 'build', 'config'], ' ');
+
+  element.classList.add('hello');
 
   return element;
 }
