1333c68bf0d585d9e3622af587611d1da347058e
easychessanimations <chessbotroulette@gmail.com>
2020-10-13 10:32:29 +0000

    Build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call npx webpack --config webpack.config.js
 start http-server dist
-c:\progra~2\Google\Chrome\Application\chrome.exe https://localhost:8080
+c:\progra~2\Google\Chrome\Application\chrome.exe http://localhost:8080
