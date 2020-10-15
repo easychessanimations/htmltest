5950db7be8e6f61657534611ead75cdc5e5d98f6
easychessanimations <chessbotroulette@gmail.com>
2020-10-15 02:56:03 +0000

    Index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css'
 import './favicon.ico'
 
-import { div, input } from './smartdom/smartdom.js'
-
-//import { div } from '@easychessanimations/foo'
+import { div, input } from './smartdom/smartdom.js' /*'@easychessanimations/foo'*/
 
 let app = div().fl().flcol().ai("center").jc("space-around").w(200).h(400).bc("#070")
             .a(input("text"),input("checkbox"),[1,2,3,4,5].map(i=>div().pad(10).w(100).h(20).bc("#007").c("#fff").tac().html(`div ${i}`)))
