536e5965c077c76bc4838cb550ee9295dcbedfb9
easychessanimations <chessbotroulette@gmail.com>
2020-10-15 02:29:22 +0000

    Move p

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import { div } from './smartdom/smartdom.js'
 //import { div } from '@easychessanimations/foo'
 
 let app = div().fl().flcol().ai("center").jc("space-around").w(200).h(400).bc("#070")
-            .a([1,2,3,4].map(i=>div().pad(10).w(100).h(20).bc("#007").c("#fff").tac().html(`div ${i}`)))
+            .a([1,2,3,4,5].map(i=>div().pad(10).w(100).h(20).bc("#007").c("#fff").tac().html(`div ${i}`)))
 
 document.body.appendChild(app.e)
